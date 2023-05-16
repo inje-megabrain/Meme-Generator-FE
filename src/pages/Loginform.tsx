44f11435c84bf6142fa9ebf9cee124f130b38a44
Dark-jin <chch4lee1@naver.com>
2023-05-16 22:28:22 +0900

    feat: google login oauth

--- src/pages/Loginform.tsx
+++ src/pages/Loginform.tsx
@@ -1,41 +1,45 @@
-import React, { useCallback } from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { SignUpAPI, loginAPI } from "../apis/auth";
+import React, { useCallback } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { SignUpAPI, loginAPI } from '../apis/auth';
+const { VITE_APP_GOOGLE_OAUTH } = import.meta.env;
 
 const Loginform = () => {
   const navigate = useNavigate();
   const [signup, setSignup] = useState(false);
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [checkEmail, setCheckEmail] = useState(false);
   const [checkPassword, setCheckPassword] = useState(false);
   const homebtn = () => {
-    navigate("/");
+    navigate('/');
+  };
+  const googlelogin = () => {
+    location.href = VITE_APP_GOOGLE_OAUTH;
   };
   const LoginFunc = async (e: any) => {
     e.preventDefault();
     if (!id) {
-      return alert("아이디를 입력해주세요");
+      return alert('아이디를 입력해주세요');
     } else if (!password) {
-      return alert("비밀번호를 입력해주세요");
+      return alert('비밀번호를 입력해주세요');
     }
     await loginAPI(id, password);
   };
   const SignupFunc = async (e: any) => {
     e.preventDefault();
     if (!id) {
-      return alert("아이디를 입력해주세요");
+      return alert('아이디를 입력해주세요');
     } else if (!password || !checkPassword) {
       return alert(
-        "비밀번호는 영문, 숫자, 특수문자를 포함하여 8자 이상 25자 이하로 입력해주세요."
+        '비밀번호는 영문, 숫자, 특수문자를 포함하여 8자 이상 25자 이하로 입력해주세요.'
       );
     } else if (!email || !checkEmail) {
-      return alert("이메일 형식이 틀렸습니다.");
+      return alert('이메일 형식이 틀렸습니다.');
     } else if (!username) {
-      return alert("이름을 입력해주세요");
+      return alert('이름을 입력해주세요');
     }
     await SignUpAPI(id, password, username, email);
   };
@@ -70,48 +74,48 @@ const Loginform = () => {
   return (
     <>
       <div>
-        <div className="btn btn-ghost normal-case text-2xl" onClick={homebtn}>
-          NyangPiece
+        <div className='btn btn-ghost normal-case text-2xl' onClick={homebtn}>
+          ME:ME
         </div>
         <div>
           <h1>User Login</h1>
         </div>
-        <div className="grid grid-rows-2 gap-5 mt-4">
+        <div className='grid grid-rows-2 gap-5 mt-4'>
           <div>
             <input
-              type="text"
-              placeholder="ID"
-              className="input w-full max-w-xs outline"
+              type='text'
+              placeholder='ID'
+              className='input w-full max-w-xs outline'
               value={id}
               onChange={(e) => setId(e.target.value)}
             />
           </div>
           <div>
             <input
-              type="password"
-              placeholder="Password"
-              className="input w-full max-w-xs outline"
+              type='password'
+              placeholder='Password'
+              className='input w-full max-w-xs outline'
               value={password}
               onChange={onChangePassword}
             />
           </div>
         </div>
         {signup ? (
-          <div className="grid grid-rows-2 gap-5 mt-4">
+          <div className='grid grid-rows-2 gap-5 mt-4'>
             <div>
               <input
-                type="text"
-                placeholder="Username"
-                className="input w-full max-w-xs outline"
+                type='text'
+                placeholder='Username'
+                className='input w-full max-w-xs outline'
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
             <div>
               <input
-                type="text"
-                placeholder="Email"
-                className="input w-full max-w-xs outline"
+                type='text'
+                placeholder='Email'
+                className='input w-full max-w-xs outline'
                 value={email}
                 onChange={onChangeEmail}
               />
@@ -119,25 +123,31 @@ const Loginform = () => {
           </div>
         ) : null}
         {!signup ? (
-          <div className="mt-4">
+          <div className='mt-4'>
             <button
-              className="btn btn-outline btn-sm outline outline-black font-bold text-sm"
+              className='btn btn-outline btn-sm outline outline-black font-bold text-sm'
               onClick={() => setSignup(true)}
             >
               회원가입
             </button>
           </div>
         ) : null}
-        <div className="grid place-items-center mt-4">
+        <div className='grid place-items-center mt-4'>
           <button
-            className="btn btn-outline outline outline-black font-bold text-2xl rounded-xl w-full max-w-xs"
+            className='btn btn-outline outline outline-black font-bold text-2xl rounded-xl w-full max-w-xs'
+            onClick={googlelogin}
+          >
+            Google
+          </button>
+          <button
+            className='btn btn-outline outline outline-black font-bold text-2xl rounded-xl w-full max-w-xs mt-4'
             onClick={LoginFunc}
           >
             Login
           </button>
           {signup ? (
             <button
-              className="btn btn-outline outline outline-black font-bold text-2xl rounded-xl w-full max-w-xs mt-4"
+              className='btn btn-outline outline outline-black font-bold text-2xl rounded-xl w-full max-w-xs mt-4'
               onClick={SignupFunc}
             >
               Sign Up
