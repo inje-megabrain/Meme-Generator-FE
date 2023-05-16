52aa136c114276e8c7c20e05ef810482c84c10ef
Dark-jin <chch4lee1@naver.com>
2023-05-16 11:13:39 +0900

    delete : wantedmodal

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,48 +1,47 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import Wantedmodal from "../../components/Wantedmodal";
-import { getCookie, removeCookie } from "../../util/Cookie";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getCookie, removeCookie } from '../../util/Cookie';
 
 const Main = () => {
   const navigate = useNavigate();
 
-  const cookie = getCookie("access_token");
+  const cookie = getCookie('access_token');
 
   const homebtn = () => {
     window.location.reload();
   };
 
   const signbtn = () => {
-    navigate("/login");
+    navigate('/login');
   };
   const logoutbtn = () => {
-    removeCookie("access_token", { path: "/" });
+    removeCookie('access_token', { path: '/' });
     window.location.reload();
   };
   const generator = () => {
-    navigate("/generator");
+    navigate('/generator');
   };
 
   return (
     <>
       <div>
         <div>
-          <div className="btn btn-ghost normal-case text-2xl" onClick={homebtn}>
+          <div className='btn btn-ghost normal-case text-2xl' onClick={homebtn}>
             ME:ME
           </div>
           {!cookie ? (
-            <div className="text-right">
+            <div className='text-right'>
               <div
-                className="btn btn-ghost normal-case text-base"
+                className='btn btn-ghost normal-case text-base'
                 onClick={signbtn}
               >
                 로그인
               </div>
             </div>
           ) : (
-            <div className="text-right">
+            <div className='text-right'>
               <div
-                className="btn btn-ghost normal-case text-base"
+                className='btn btn-ghost normal-case text-base'
                 onClick={logoutbtn}
               >
                 로그아웃
@@ -50,18 +49,18 @@ const Main = () => {
             </div>
           )}
         </div>
-        <div className="grid grid-cols-2 gap-3">
+        <div className='grid grid-cols-2 gap-3'>
           <div>
             <label
-              htmlFor="my-modal-1"
-              className="btn btn-ghost text-base font-bold"
+              htmlFor='my-modal-1'
+              className='btn btn-ghost text-base font-bold'
             >
               수배
             </label>
           </div>
           <div>
             <button
-              className="btn btn-ghost text-base font-bold"
+              className='btn btn-ghost text-base font-bold'
               onClick={generator}
             >
               짤 생성
@@ -69,7 +68,6 @@ const Main = () => {
           </div>
         </div>
       </div>
-      <Wantedmodal modalnumber="my-modal-1" />
     </>
   );
 };
