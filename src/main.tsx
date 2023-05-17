3052696f5625d50d6276f601563c03741c98a9cd
Dark-jin <chch4lee1@naver.com>
2023-05-17 20:58:13 +0900

    react-toastify

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style/global.css';
 import { RecoilRoot } from 'recoil';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const { VITE_APP_KAKAO_KEY } = import.meta.env;
 const kakaokey = VITE_APP_KAKAO_KEY;
@@ -16,6 +18,7 @@ let counter = setInterval(() => {
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RecoilRoot>
+    <ToastContainer />
     <App />
   </RecoilRoot>
 );
