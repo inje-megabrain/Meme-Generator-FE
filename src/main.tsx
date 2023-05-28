862df10ddb3865deaa5b50694ffd9b823b2093e4
Dark-jin <chch4lee1@naver.com>
2023-05-28 19:52:39 +0900

    feat: footer

--- src/main.tsx
+++ src/main.tsx
@@ -20,5 +20,10 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RecoilRoot>
     <ToastContainer />
     <App />
+    <footer className='bottom-0 footer footer-center p-4 text-base-content mt-10'>
+      <div>
+        <p>Copyright © 2023 - Megabrain Meme-Generator</p>
+      </div>
+    </footer>
   </RecoilRoot>
 );
